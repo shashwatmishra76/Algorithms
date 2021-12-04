94b95bcefe5ab02631c861233b6b299a0a6696dd
Shashwat Mishra <shashwatmishra76@gmail.com>
2021-12-04 14:11:01 +0530

    Update EuclidGcdAlgorithm.cpp

--- EuclidGcdAlgorithm.cpp
+++ EuclidGcdAlgorithm.cpp
@@ -26,8 +26,8 @@ int extendedGcdEuclid(int a, int b, int *x, int *y){
     
     int x1, y1;
     int gcd = extendedGcdEuclid(b % a, a, &x1, &y1);
-    *x = y1;
-    *y = x1 - ((a / b) * y1);
+    *x = y1 - ((b / a) * x1);
+    *y = x1;
     
     return gcd;
 }
